9b47abd41553da18463e720189990f745c58fda5
veronica <verdani8@gmail.com>
2022-07-28 20:58:15 -0300

    webconfig added

--- vdr/src/main/java/com/portfolioveroramirez/vdr/Controller/PersonaController.java
+++ vdr/src/main/java/com/portfolioveroramirez/vdr/Controller/PersonaController.java
@@ -5,7 +5,7 @@ import com.portfolioveroramirez.vdr.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://portfoliovdr.web.app/")
+
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
